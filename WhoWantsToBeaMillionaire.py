2961bfe396c4299217ec2ae0c62309369b05440f
MooseTaesh <jycalv95@gmail.com>
2023-07-05 18:06:27 +0100

    Fixing some bugs

--- WhoWantsToBeaMillionaire.py
+++ WhoWantsToBeaMillionaire.py
@@ -8,44 +8,44 @@ answers = ["C","B","C","D","A"]
 bank = 0
 name = str(input("Hello and welcome to who wants to be a millionaire! What is your name? "))
 print("Thanks",name)
-ready = str(input("Are you ready? Y/N "))
+ready = str(input("Are you ready? Y/N ")).upper()
 if ready == "Y":
     print("Ok here we go")
     print("Question 1 for £100")
     print(questions[0])
     print("A Money, B Faith, C 42, D Fun")
-    q1 = str(input("What is your answwer? "))
+    q1 = str(input("What is your answer? ")).upper()
     if q1 == answers[0]:
      print("Correct! Adding £100 to the bank")
-     bank = bank + 100
+     bank += 100
      print("The next question is for £1000")
      print(questions[1])
      print("A Huw Jackman, B Nathan Fillion, C Idris Elba, D Chris Hemsworth")
-     q2 = str(input("What is your answer? "))
+     q2 = str(input("What is your answer? ")).upper()
      if q2 == answers[1]:
         print("Correct! Adding £1000 to the bank!")
-        bank = bank + 1000
-        print("The next question is for £10000")
+        bank += 1000
+        print("The next question is for £10000")cleer
         print(questions[2])
         print("A Lemon, B Chocolate, C Clemintine, D Pineapple")
-        q3 = str(input("What is your answer? "))
+        q3 = str(input("What is your answer? ")).upper()
         if q3 == answers[2]:
            print("Correct! Adding £10000 to the bank!")
-           bank = bank + 10000
+           bank += 10000
            print("The next question is for £100000")
            print(questions[3])
            print("A Mast, B Tifu, C Ninja, D Babish")
-           q4 = str(input("What is your answer? "))
+           q4 = str(input("What is your answer? ")).upper()
            if q4 == answers[3]:
               print("Correct! Adding £100000 to the bank!")
-              bank = bank + 100000
+              bank += 100000
               print("This is the last question! It is for £1000000")
               print(questions[4])
               print("A Copper, B Tumbler, C Collins, D French")
-              q5 = str(input("What is your final answer? "))
+              q5 = str(input("What is your answer? ")).upper()
               if q5 == answers[4]:
                  print("That is correct! You have answered all the questions right!")
-                 bank = bank + 1000000
+                 bank += 1000000
                  print("You will be taking home £",bank)
                  print("Thank you for playing!!!")
               else:
