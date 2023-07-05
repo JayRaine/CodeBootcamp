1d2cd70d9c1e43346903ff679f35a0022f1d615c
MooseTaesh <jycalv95@gmail.com>
2023-07-05 09:41:37 +0100

    Initial commit

---
+++ 1stHomework.py
@@ -0,0 +1,44 @@
+# Activity 1
+my_intiger= 10
+print(my_intiger)
+my_float = 3.4
+print(my_float)
+my_string = "My String"
+print(my_string)
+
+# Activity 2
+name = "Jay"
+print("Hello, ",name)
+sentence = "This is a test for the Len() function"
+print(len(sentence))
+phrase = "Bing Pot!!"
+x = phrase.upper()
+print(x)
+
+# Activity 3
+num1 = 10
+num2 = 20
+print(num1 + num2)
+num3 = 5
+num4 = 4
+print(num3 * num4)
+num5 = 15
+num6 = 3
+print(num5 / num6)
+
+# Activity 4
+age = 28
+if age > 18:
+    print("You are an adult")
+else: 
+    print("You are not an adult")
+num = -42
+if num > 0:
+    print("The number is positive")
+else:
+    print("The number is not positive")
+password ="321wordpass"
+if password == "password123":
+    print("Access granted")
+else: 
+    print("Access denied")
