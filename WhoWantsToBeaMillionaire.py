e5806e133343a5d38e2231f9fba6eff7f6a6ecba
MooseTaesh <jycalv95@gmail.com>
2023-07-05 17:32:51 +0100

    I added more questions

--- WhoWantsToBeaMillionaire.py
+++ WhoWantsToBeaMillionaire.py
@@ -1,13 +1,68 @@
 # Create a program capable of displaying questions to the user like Who Wants to Be a Millionaire? 
 # Use List data type to store the questions and their correct answers. 
 # Display the final amount the person is taking home after playing the game.
-question = "What is the answer to everything?"
-print(question)
-choices = ["A Money","B Faith","C 42","D Fun"]
-print(choices)
-a = str(input("What is your answer? "))
-print(a)
-if (a=="C" or a=="42"):
-    print("That is corrrect! The answer to everything is 42!")
-else: print("Sorry that is not correct!")
+
+questions = ["What is the answer to everything?","Who plays cade-6?","What cake does walter mitty make?","Andrew Rea is otherwise known as?","What mug is a moscow mule served in?"]
+#C 42, B Nathan Fillion, C Clementine, D Babish, A Copper
+answers = ["C","B","C","D","A"] 
+bank = 0
+name = str(input("Hello and welcome to who wants to be a millionaire! What is your name? "))
+print("Thanks",name)
+ready = str(input("Are you ready? Y/N "))
+if ready == "Y":
+    print("Ok here we go")
+    print("Question 1 for £100")
+    print(questions[0])
+    print("A Money, B Faith, C 42, D Fun")
+    q1 = str(input("What is your answwer? "))
+    if q1 == answers[0]:
+     print("Correct! Adding £100 to the bank")
+     bank = bank + 100
+     print("The next question is for £1000")
+     print(questions[1])
+     print("A Huw Jackman, B Nathan Fillion, C Idris Elba, D Chris Hemsworth")
+     q2 = str(input("What is your answer? "))
+     if q2 == answers[1]:
+        print("Correct! Adding £1000 to the bank!")
+        bank = bank + 1000
+        print("The next question is for £10000")
+        print(questions[2])
+        print("A Lemon, B Chocolate, C Clemintine, D Pineapple")
+        q3 = str(input("What is your answer? "))
+        if q3 == answers[2]:
+           print("Correct! Adding £10000 to the bank!")
+           bank = bank + 10000
+           print("The next question is for £100000")
+           print(questions[3])
+           print("A Mast, B Tifu, C Ninja, D Babish")
+           q4 = str(input("What is your answer? "))
+           if q4 == answers[3]:
+              print("Correct! Adding £100000 to the bank!")
+              bank = bank + 100000
+              print("This is the last question! It is for £1000000")
+              print(questions[4])
+              print("A Copper, B Tumbler, C Collins, D French")
+              q5 = str(input("What is your final answer? "))
+              if q5 == answers[4]:
+                 print("That is correct! You have answered all the questions right!")
+                 bank = bank + 1000000
+                 print("You will be taking home £",bank)
+                 print("Thank you for playing!!!")
+              else:
+                 print("Sorry you were wrong you leave with £",bank)
+                 print("Thank you for playing!!!")   
+           else:
+              print("Sorry you were wrong you leave with £",bank)
+              print("Thank you for playing!!!")    
+        else:
+           print("Sorry you were wrong you leave with £",bank)
+           print("Thank you for playing!!!")      
+     else:
+        print("Sorry you were wrong you leave with £",bank)
+    else: 
+       print("Sorry you were wrong you leave with £",bank)
+       print("Thank you for playing!!!")
+else:
+   print("Sorry, see you again soon!")
+
 
